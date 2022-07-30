74f268c8e4fcc1a9b817efe5b6b5290195bd8758
Filip Zagozdon <filipzagozdon8@gmail.com>
2022-07-30 04:54:26 +0200

    Implement grid resizing

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
-const gridSize = 960;
+const STARTING_GRID_SIDE_SIZE = 32;
+const MAX_GRID_SIZE = 64;
 
 const gridContainer = document.querySelector(".grid-container");
-initGrid(gridContainer, 32);
+resizeGrid(gridContainer, STARTING_GRID_SIDE_SIZE);
 
-const squareGrids = document.querySelectorAll(".grid-square");
-addHoveringEffect(squareGrids);
+const resizeButton = document.querySelector("#resize");
+resizeButton.addEventListener("click", (e) => {
+    const userSize = +prompt("Enter desired grid size: ");
 
-function initGrid(gridContainer, gridSideSize) {
-    gridContainer.style.display = "grid";
+    if (!validateUserNumberInput(userSize)) {
+        alert("Invalid size has been entered!\nEnter an integer in the range of 1-64");
+        return;
+    }
+
+    resizeGrid(gridContainer, userSize);
+});
+
+function resizeGrid(gridContainer, gridSideSize) {
+    gridContainer.textContent = '';
     gridContainer.style.gridTemplateColumns = '1fr '.repeat(gridSideSize);
-    //gridContainer.setAttribute("display", "flex");
-    //gridContainer.setAttribute("grid-template-columns", '1fr '.repeat(gridSideSize));
 
     for (let i = 0; i < gridSideSize ** 2; i++) {
         const square = document.createElement("div");
         square.classList.add("grid-square");
         gridContainer.appendChild(square);
     }
+
+    const squareGrids = document.querySelectorAll(".grid-square");
+    addHoveringEffect(squareGrids);
 }
 
 function addHoveringEffect(squares) {
@@ -25,4 +36,12 @@ function addHoveringEffect(squares) {
             square.classList.add("colored");
         }))
     })
+}
+
+function validateUserNumberInput(input) {
+    if (Number.isInteger(input) && input >= 1 && input <= MAX_GRID_SIZE) {
+        return true;
+    }
+
+    return false;
 }
